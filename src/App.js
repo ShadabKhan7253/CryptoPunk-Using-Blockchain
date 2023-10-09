27f7d643d4ef10311b0a7fe2e08732d97ed9b4bb
Shadab Khan <shadabkhan7253@gmail.com>
2023-10-09 20:18:00 +0530

    header completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return <h1>Welcome to React</h1>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
